9de40f7f50c281fbf9760b274fe75037c6548d2e
Oleg Blacksmith <OKuznetsov21041992@outlook.com>
2025-10-24 18:04:36 +0400

    add: layouts

---
+++ app/layouts/about.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import FooterComponent from '../components/FooterComponent.vue';
+import HeaderComponent from '../components/HeaderComponent.vue';
+import { navAnchors } from '../../data/nav-anchors'
+import { navPages } from '../../data/nav-pages'
+
+const homeNavPage = navPages[1] ? [navPages[1]] : [];
+
+</script>
+
+<template>
+
+  <HeaderComponent 
+    :nav-pages="homeNavPage"
+  />
+  <main>
+      <NuxtPage />
+  </main>
+  <FooterComponent />
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ app/layouts/default.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { provide, ref } from 'vue';
+import FooterComponent from '../components/FooterComponent.vue';
+import HeaderComponent from '../components/HeaderComponent.vue';
+import SuccessfulDialogue from '../components/SuccessfulDialog.vue';
+import ErrorDialog from '../components/ErrorDialog.vue';
+import { navAnchors } from '../../data/nav-anchors'
+import { navPages } from '../../data/nav-pages'
+
+const firstNavPage = navPages[0] ? [navPages[0]] : [];
+ 
+const isSuccessDialogVisible = ref(false);
+const isErrorDialogVisible = ref(false);
+
+function openSuccessDialog() {
+  isSuccessDialogVisible.value = true;
+  document.body.style.overflow = 'hidden';
+}
+
+function openErrorDialog() {
+  isErrorDialogVisible.value = true;
+  document.body.style.overflow = 'hidden';
+}
+
+function closeSuccessDialog() {
+  isSuccessDialogVisible.value = false;
+  document.body.style.overflow = '';
+}
+
+function closeErrorDialog() {
+  isErrorDialogVisible.value = false;
+  document.body.style.overflow = '';
+}
+
+provide('isSuccessDialogVisible', isSuccessDialogVisible);
+provide('isErrorDialogVisible', isErrorDialogVisible);
+
+provide('openSuccessDialog', openSuccessDialog);
+provide('openErrorDialog', openErrorDialog);
+
+provide('closeSuccessDialog', closeSuccessDialog);
+provide('closeErrorDialog', closeErrorDialog);
+
+</script>
+
+<template>
+
+  <HeaderComponent 
+    :nav-anchor="navAnchors"
+    :nav-pages="firstNavPage"
+  />
+  <main>
+      <NuxtPage />
+  </main>
+  <SuccessfulDialogue />
+  <ErrorDialog />
+  <FooterComponent />
+
+</template>
+
+<style scoped>
+
+</style>
